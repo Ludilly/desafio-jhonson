c3d2d44d1715d7c3de2b29b3ba98f24187c40b34
ludilly <luiza.dilly@gmail.com>
2025-12-11 22:07:34 -0300

    ajuste front

--- client/src/app/components/PersonDetails.tsx
+++ client/src/app/components/PersonDetails.tsx
@@ -8,10 +8,22 @@ interface Props {
 export default function PersonDetails({ person }: Props) {
   return (
     <Box sx={{ pl: 4, py: 2 }}>
-      <Typography>Department: {person.department}</Typography>
-      <Typography>Manager ID: {person.managerId ?? "None"}</Typography>
-      <Typography>Type: {person.type}</Typography>
-      <Typography>Status: {person.status}</Typography>
+      <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+        <Typography sx={{ fontWeight: 700, marginRight: '4px' }}>Department: </Typography>
+        <Typography>{person.department}</Typography>
+      </Box>
+      <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+        <Typography sx={{ fontWeight: 700, marginRight: '4px' }}>Job Position: </Typography>
+        <Typography>{person.jobTitle}</Typography>
+      </Box>
+      <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+        <Typography sx={{ fontWeight: 700, marginRight: '4px' }}>Type:</Typography>
+        <Typography>{person.type}</Typography>
+      </Box>
+      <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+        <Typography sx={{ fontWeight: 700, marginRight: '4px' }}>Status:</Typography>
+        <Typography>{person.status}</Typography>
+      </Box>
     </Box>
   );
 }
